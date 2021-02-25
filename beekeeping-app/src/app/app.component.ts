246acb7173999868cdc14feea081463b71fcdfee
simonasjur <simonasjur@gmail.com>
2021-02-25 19:57:57 +0200

    client authentication

--- beekeeping-app/src/app/app.component.ts
+++ beekeeping-app/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+import { AuthenticationService } from './_services';
+import { User } from './_models';
+
+@Component({ selector: 'app', templateUrl: 'app.component.html' })
 export class AppComponent {
-  title = 'beekeeping-app';
+    title = 'Beekeeping app'
+    user: User;
+
+    constructor(
+        private router: Router,
+        private authenticationService: AuthenticationService
+    ) {
+        this.authenticationService.user.subscribe(x => this.user = x);
+    }
+
+    logout() {
+        this.authenticationService.logout();
+    }
 }
